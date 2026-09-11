d802b50ba3357d68f2f3858c1e9a4eccaa4f1c19
Matthias Harzer <matthias.harzer03@gmail.com>
2026-09-11 15:23:51 +0200

    fix: stop writing clip when client disconnects

--- cmd/twitch/run/api/v1/list/response.go
+++ cmd/twitch/run/api/v1/list/response.go
@@ -7,11 +7,13 @@ type Response struct {
 }
 
 type ResponseStream struct {
-	ID                  string    `json:"id"`
-	Title               string    `json:"title"`
-	Size                string    `json:"size"`
-	Duration            string    `json:"duration"`
-	StartedAt           time.Time `json:"started_at"`
-	BroadcasterUserName string    `json:"broadcaster_user_name"`
-	StreamState         string    `json:"stream_state"`
+	ID                   string    `json:"id"`
+	Title                string    `json:"title"`
+	Size                 string    `json:"size"`
+	SizeBytes            int64     `json:"size_bytes"`
+	Duration             string    `json:"duration"`
+	DurationMilliseconds int64     `json:"duration_milliseconds"`
+	StartedAt            time.Time `json:"started_at"`
+	BroadcasterUserName  string    `json:"broadcaster_user_name"`
+	StreamState          string    `json:"stream_state"`
 }
